9310a578e438e4daf986989de74a099bed0666bb
Prashant Trivedi <prashant.tri007@gmail.com>
2015-04-23 02:01:50 -0400

    Utils

--- Project2/src/Utils.java
+++ Project2/src/Utils.java
@@ -1,8 +1,69 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 
 public class Utils {
 	public static int calcChecksum(byte[] data){
 		int result = 0;
-		
+
 		return result;
+	}
+
+	public static Object deserializePacket(byte[] packetData)
+	{
+		try
+		{
+			ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(packetData));
+			Object obj = (Object) in.readObject();
+			in.close();
+			return obj;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static byte[] serializePacket(DataPacket packet)
+	{
+	    try
+	    {
+	      ByteArrayOutputStream os = new ByteArrayOutputStream(2048);
+	      ObjectOutputStream outs = new ObjectOutputStream(os);
+	      outs.writeObject(packet);
+	      outs.close();
+	      byte[] byte_arr= os.toByteArray();
+	      os.close();
+	      return byte_arr;
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return null;
+
+	}
+	
+	public static byte[] serializeAck(AckHeader ack)
+	{
+	    try
+	    {
+	      ByteArrayOutputStream os = new ByteArrayOutputStream(2048);
+	      ObjectOutputStream outs = new ObjectOutputStream(os);
+	      outs.writeObject(ack);
+	      outs.close();
+	      byte[] byte_arr= os.toByteArray();
+	      os.close();
+	      return byte_arr;
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return null;
+
 	}
 }
