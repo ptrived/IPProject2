4d71dec60e0c7935bd8cba1ac99509a553d17dc3
Prashant Trivedi <prashant.tri007@gmail.com>
2015-04-26 23:10:57 -0400

    rename

--- Project2/src/SimpleFTPClient.java
+++ Project2/src/SimpleFTPClient.java
@@ -5,58 +5,68 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.TreeMap;
 
-/**
- * 
- * Timer Task for Resending Packet
- *
- */
-class GoBackNTimerTask extends TimerTask{
+/*class SimpleClientWindow{
+	DataPacket data;
+	Boolean ackRcvd;
+	public SimpleClientWindow(DataPacket data){
+		this.data =  data;
+		ackRcvd = false;
+	}
+	public DataPacket getData() {
+		return data;
+	}
+	public void setData(DataPacket data) {
+		this.data = data;
+	}
+	public Boolean getAckRcvd() {
+		return ackRcvd;
+	}
+	public void setAckRcvd(Boolean ackRcvd) {
+		this.ackRcvd = ackRcvd;
+	}
 
+}*/
+
+class GoBackNTimerTaskNew extends TimerTask{
 	@Override
 	public void run() {	
-
-		System.out.println("Timeout, sequence number = "+SimpleFTPClient.window.get(0).getSequenceNumber());
-		for(int i=0; i<SimpleFTPClient.windowSize; i++){
-			if(i >= SimpleFTPClient.window.size()){
-				break;
-			}
-			DataPacket packet = SimpleFTPClient.window.get(i);
-			packet.setChecksum(Utils.calcChecksum(packet));
-			byte[] dataArr = Utils.serializePacket(packet);
-			InetAddress ipAddr;
-			try {
-				ipAddr = InetAddress.getByName(SimpleFTPClient.serverHostname);
-				DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,SimpleFTPClient.portNum);
-				SimpleFTPClient.client.send(dataPacket);				
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+		synchronized(Client.window){
+			System.out.println("Timeout, sequence number = "+Client.window.firstKey());
+			int num = Client.window.firstKey();
+			int count = 0;
+			while(count < Client.windowSize && Client.window.containsKey(num)){
+				DataPacket packet = Client.window.get(num);
+				byte[] dataArr = Utils.serializePacket(packet);
+				InetAddress ipAddr;
+				try {
+					ipAddr = InetAddress.getByName(Client.serverHostname);
+					DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,Client.portNum);
+					Client.client.send(dataPacket);		
+					num += Client.MSS;
+					count++;
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
-
 }
 
-/**
- * Class for GoBackN Client:
- * command to start the client: SimpleFTPClient server_host_name server-port file-name N MSS
- * 
- */
-public class SimpleFTPClient implements Runnable{
+public class Client implements Runnable {
 	static int portNum=7735;
 	static String serverHostname;
 	static DatagramSocket client;
-	static SimpleFTPClient simpleClient;
+	static Client simpleClient;
 	static String filename;
 	static int windowSize ;
 	static int MSS;
-	static List<DataPacket> window ;
+	static TreeMap<Integer, DataPacket> window ;
 	static byte[] mssData;
 	static int mssCount;
 	static int lastAckRcvd;
@@ -69,98 +79,51 @@ public class SimpleFTPClient implements Runnable{
 	static Timer timer;
 
 	/**
-	 * 
-	 *  Reads data byte by byte from the file
-	 *  Buffers the data locally 
-	 *  Sends one packet when it has MSS bytes of data
-	 *  Sends Header + MSS bytes of data ; except for the last packet
-	 *  Implement Timeout for lost ACKs
-	 *  
+	 * Initialize variables
 	 */
-	private static int goBackN(byte buff[], boolean lastFlag){
-		if(lastFlag == true && mssCount > 0){
-			//send the last packet
-			mssCount = 0;
-			lastPktSent++;
-
-			DataPacket data = new DataPacket(mssData);
-			data.setSequenceNumber(sequenceNum);
-			data.setChecksum(Utils.calcChecksum(data));
-			sequenceNum = sequenceNum+MSS;
-			window.add(data);
-			if(window.size() < windowSize){
-				byte[] dataArr = Utils.serializePacket(data);
-				InetAddress ipAddr;
-				try {
-					ipAddr = InetAddress.getByName(serverHostname);
-					DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
-					client.send(dataPacket);
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			//send Last packet with data 0
-			mssData = new byte[0];
-			data = new DataPacket(mssData);
-			endAckExpected = sequenceNum+MSS;
-			data.setSequenceNumber(sequenceNum);
-			data.setChecksum(Utils.calcChecksum(data));
-			sequenceNum = sequenceNum+MSS;
-			window.add(data);
-			if(window.size() < windowSize){
-				byte[] dataArr = Utils.serializePacket(data);
-				InetAddress ipAddr;
-				try {
-					ipAddr = InetAddress.getByName(serverHostname);
-					DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
-					client.send(dataPacket);
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		else{
-			mssData[mssCount++] = buff[0];
-			if(mssCount==MSS){
-				DataPacket data = new DataPacket(mssData);
-				data.setSequenceNumber(sequenceNum);
-				data.setChecksum(Utils.calcChecksum(data));
-				sequenceNum = sequenceNum+MSS;
-				window.add(data);
-				mssCount = 0;
-				lastPktSent++;		
-				mssData = new byte[MSS];
-				if(window.size()<= windowSize){
-					byte[] dataArr = Utils.serializePacket(data);
-					InetAddress ipAddr;
-					try {
-						ipAddr = InetAddress.getByName(serverHostname);
-						DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
-						client.send(dataPacket);
-					} catch (UnknownHostException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}			
-				}
-				if(lastPktSent==0){
-					timerTask = new GoBackNTimerTask();
-					timer = new Timer(true);
-					timer.scheduleAtFixedRate(timerTask,100, 100);
-				}
-			}
-		}
-		return 1;
+	private static void init() {
+		mssData = new byte[MSS];
+		window = new TreeMap<Integer, DataPacket>();
+		mssCount = 0;
+		lastAckRcvd = -1;
+		lastPktSent = -1;
+		firstPktInWindow = 0;
+		endAckExpected = -1;
+		Thread t = new Thread(simpleClient);
+		t.start();
 	}
 
-	/**
-	 * Main method to send the packets
-	 */
+	public static void main(String[] args) {
+		try{
+			serverHostname = args[0];
+			int portInput = Integer.parseInt(args[1]);
+			if(portInput!=portNum){
+				System.out.println("Invalid Server Port Number");
+				System.exit(1);
+			}
+			filename = args[2];
+			windowSize = Integer.parseInt(args[3]);
+			MSS = Integer.parseInt(args[4]);
+			//			serverHostname = "localhost";
+			//			portNum = 7735;
+			//			filename = "123.txt";
+			//			windowSize = 16;
+			//			MSS = 1000;
+			sequenceNum =0;
+			client = new DatagramSocket();
+			System.out.println("Connected to server");
+			simpleClient = new Client();
+			init();
+			startTime = System.currentTimeMillis();
+			rdt_send();			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+
+
 	private static void rdt_send(){
 		FileInputStream input = null;
 		try {
@@ -188,55 +151,101 @@ public class SimpleFTPClient implements Runnable{
 		}
 	}
 
-	/**
-	 * 
-	 * main method for Client
-	 */
-	public static void main (String[] args){
-		try{
-			serverHostname = args[0];
-			int portInput = Integer.parseInt(args[1]);
-			if(portInput!=portNum){
-				System.out.println("Invalid Server Port Number");
-				System.exit(1);
-			}
-			filename = args[2];
-			windowSize = Integer.parseInt(args[3]);
-			MSS = Integer.parseInt(args[4]);
-			//			serverHostname = "localhost";
-			//			portNum = 7735;
-			//			filename = "123.txt";
-			//			windowSize = 16;
-			//			MSS = 1000;
-			sequenceNum =0;
-			client = new DatagramSocket();
-			System.out.println("Connected to server");
-			simpleClient = new SimpleFTPClient();
-			init();
-			startTime = System.currentTimeMillis();
-			rdt_send();			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
 
-	/**
-	 * Initialize variables
-	 */
-	private static void init() {
-		mssData = new byte[MSS];
-		window = new ArrayList<DataPacket>();
-		mssCount = 0;
-		lastAckRcvd = -1;
-		lastPktSent = -1;
-		firstPktInWindow = 0;
-		endAckExpected = -1;
-		Thread t = new Thread(simpleClient);
-		t.start();
+	private static void goBackN(byte[] buff, boolean lastFlag) {
+
+		if(lastFlag == true && mssCount > 0){
+			//send the last packet
+			mssCount = 0;
+			lastPktSent++;
+
+			DataPacket data = new DataPacket(mssData);
+			data.setSequenceNumber(sequenceNum);
+			window.put(sequenceNum, data);
+			data.setChecksum(Utils.calcChecksum(data));
+			sequenceNum = sequenceNum+MSS;
+
+
+			//window.add(data);
+			if(window.size() < windowSize){
+				byte[] dataArr = Utils.serializePacket(data);
+				InetAddress ipAddr;
+				try {
+					ipAddr = InetAddress.getByName(serverHostname);
+					DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
+					client.send(dataPacket);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			//send Last packet with data 0
+			mssData = new byte[0];
+			data = new DataPacket(mssData);
+			endAckExpected = sequenceNum+MSS;
+			data.setSequenceNumber(sequenceNum);
+			data.setChecksum(Utils.calcChecksum(data));
+			window.put(sequenceNum, data);
+			sequenceNum = sequenceNum+MSS;
+			//element = new SimpleClientWindow(data);
+
+			if(window.size() < windowSize){
+				byte[] dataArr = Utils.serializePacket(data);
+				InetAddress ipAddr;
+				try {
+					ipAddr = InetAddress.getByName(serverHostname);
+					DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
+					client.send(dataPacket);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		else{
+			mssData[mssCount++] = buff[0];
+			if(mssCount==MSS){
+				DataPacket data = new DataPacket(mssData);
+				data.setSequenceNumber(sequenceNum);
+				data.setChecksum(Utils.calcChecksum(data));
+				window.put(sequenceNum, data);
+				sequenceNum = sequenceNum+MSS;
+				//SimpleClientWindow element = new SimpleClientWindow(data);
+
+				mssCount = 0;
+				lastPktSent++;		
+				mssData = new byte[MSS];
+				if(window.size()<= windowSize){
+					byte[] dataArr = Utils.serializePacket(data);
+					InetAddress ipAddr;
+					try {
+						ipAddr = InetAddress.getByName(serverHostname);
+						DatagramPacket dataPacket = new DatagramPacket(dataArr, dataArr.length,ipAddr,portNum);
+						client.send(dataPacket);
+					} catch (UnknownHostException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}			
+				}
+				if(lastPktSent==0){
+					timerTask = new GoBackNTimerTaskNew();
+					timer = new Timer(true);
+					timer.scheduleAtFixedRate(timerTask,100, 100);
+				}
+			}
+		}
+		//return 1;
+
+
 	}
 
 	@Override
 	public void run() {
+
 		try {
 			while(true){
 				byte[] buffer = new byte[1000];
@@ -244,23 +253,24 @@ public class SimpleFTPClient implements Runnable{
 				client.receive(ackPacket);
 				AckHeader ackData = (AckHeader) Utils.deserializePacket(ackPacket.getData());
 				int ackRcvd = ackData.getSequenceNumber();
+				//System.out.println("Recv ack : " + ackRcvd + " last ack rcvd :" + lastAckRcvd + " firstpkt :" + firstPktInWindow  + " window size :" + window.size() );
 				if(ackRcvd == endAckExpected ){
 					long endTime = System.currentTimeMillis();
-					float time = (endTime - SimpleFTPClient.startTime);
+					float time = (endTime - Client.startTime);
 					System.out.println("File transfer completed \nTime to transfer : "+(time/1000)+" Sec");					
 					System.exit(1);
 				}
-				if(ackRcvd > lastAckRcvd){
+				if(ackRcvd > window.firstKey()){
 					lastAckRcvd = ackRcvd;
 					timer.cancel();
-					//slide the window
-					//synchronized (window) {
-					while(firstPktInWindow < lastAckRcvd && window.size()>0){
-						//System.out.println("removing " + firstPktInWindow + " from window = " + window.get(0).getSequenceNumber());
-						window.remove(0);
-						firstPktInWindow+=MSS;
+					synchronized (window) {
+						while(firstPktInWindow < lastAckRcvd && window.size()>0){
+
+							window.remove(firstPktInWindow);
+							firstPktInWindow = window.firstKey();
+						}
 					}
-					timerTask = new GoBackNTimerTask();
+					timerTask = new GoBackNTimerTaskNew();
 					timer = new Timer(true);
 					timer.scheduleAtFixedRate(timerTask, 100, 100);
 				}
